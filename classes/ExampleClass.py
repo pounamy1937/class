d42f5e6c776b60284e3262af466b99c96c932f6b
pounamy1937 <pounamy1937@go.kilgore.edu>
2020-10-20 18:08:53 +0000

    Initial commit

---
+++ classes/ExampleClass.py
@@ -0,0 +1,7 @@
+class ExampleClass:
+  __privateVariable1 = "private variable A"
+  __privateVariable2 = "private variable B"
+
+  def __init__(self, nameValue, ageValue):
+    self.name = nameValue
+    self.age = ageValue
---
+++ main.py
@@ -0,0 +1,7 @@
+from classes.ExampleClass import ExampleClass
+
+exampleClass = ExampleClass("Amy", 23)
+print(exampleClass.name)
+exampleClass.age = 300
+exampleClass.name = "Seryna"
+print(exampleClass.name, exampleClass.age)
